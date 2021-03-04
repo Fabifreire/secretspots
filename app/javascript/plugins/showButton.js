8674fb1d34369087173e207c4286a9a7680384d5
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-03-04 20:40:04 +0000

    stupid buttn

--- app/javascript/plugins/showButton.js
+++ app/javascript/plugins/showButton.js
@@ -1,12 +1,15 @@
 const showButton = () => {
   window.addEventListener('scroll', event => {
     const up = document.querySelector('.up');
-    console.log(up);
+    // console.log(up);
     up.classList.add('show');
-    console.log(event);
-    console.log(event.srcElement.scrollingElement.scrollTop);
+    // console.log(event);
+    // console.log(event.srcElement.scrollingElement.scrollTop);
     const height = event.srcElement.scrollingElement.scrollTop;
-    if (height < 175) {
+    const footer = document.querySelector('footer');
+    const result = (height - footer.offsetHeight)
+    // console.log(footer.offsetHeight, result)
+    if (175 > height) {
       up.classList.remove('show');
     }
   });
