168d0c9ea6e930c0d988189c5f718750ee89a822
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-03-08 14:41:25 +0000

    erase seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-category = %w[monument mirador panoramic beach bay cliff]
+# category = %w[monument mirador panoramic beach bay cliff]
 puts "cleaning the database..."
 Like.destroy_all
 Review.destroy_all
 Spot.destroy_all
 User.destroy_all
 
-puts "generating 5 users"
-5.times do
-  puts "."
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Name.name, email: Faker::Internet.email, password: '123456')
-end
-puts "done"
+# puts "generating 5 users"
+# 5.times do
+#   puts "."
+#   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Name.name, email: Faker::Internet.email, password: '123456')
+# end
+# puts "done"
 
-puts "generating 10 spots"
+# puts "generating 10 spots"
 
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Cascais", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Carcavelos", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Parede", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Oeiras", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Lisboa", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "barcelona", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Madrid", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Paris", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Rome", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "London", category: category.sample)
-Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Rua do Instituto Bacteriológico 8, Lisboa", category: category.sample)
-puts "Success!"
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Cascais", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Carcavelos", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Parede", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Oeiras", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Lisboa", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "barcelona", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Madrid", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Paris", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Rome", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "London", category: category.sample)
+# Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Rua do Instituto Bacteriológico 8, Lisboa", category: category.sample)
+# puts "Success!"
 
-puts "generating 30 reviews"
-30.times do
-  Review.create(user: User.all.sample, spot: Spot.all.sample, description: Faker::Quotes::Shakespeare.hamlet_quote, title: Faker::Name.name, rating: rand(0..5))
-end
-puts "Success!"
+# puts "generating 30 reviews"
+# 30.times do
+#   Review.create(user: User.all.sample, spot: Spot.all.sample, description: Faker::Quotes::Shakespeare.hamlet_quote, title: Faker::Name.name, rating: rand(0..5))
+# end
+# puts "Success!"
 
 
-puts "generating 100 likes"
-100.times do
-  Like.create(user: User.all.sample, review: Review.all.sample)
-end
+# puts "generating 100 likes"
+# 100.times do
+#   Like.create(user: User.all.sample, review: Review.all.sample)
+# end
 puts "Success!"
