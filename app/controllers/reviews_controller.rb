52b285bc1a739e0bb381ff07336f91aeb952c0fb
Fabio Freire <fabitwink@hotmail.com>
2021-03-09 16:20:38 +0000

    STUPID REVIEWS CAN BE DELETED NOW

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(reviews_params)
+    
     @review.spot = @spot
     @review.user = current_user
     if @review.save
@@ -14,8 +15,18 @@ class ReviewsController < ApplicationController
     else
       render "new"
     end
-
+  end
+  
   def destroy
+    @review = Review.find(params[:id])
+     raise
+     # if @review.user == current_user || current_user.moderator?
+      @review.destroy
+      redirect_to spot_path(@review.spot)
+     #else
+      #flash.now[:alert] = "Sorry, you dont have that permission."
+     # render "spots/show"
+     #end
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- category = %w[monument mirador panoramic beach bay cliff]
-puts "cleaning the database..."
+ # category = %w[monument mirador panoramic beach bay cliff]
+# puts "cleaning the database..."
 # Like.destroy_all
-# Review.destroy_all
-# Spot.destroy_all
-User.destroy_all
+# User.destroy_all
 puts "creating moderator"
 User.create(first_name: "Mr", last_name: "Moderator", username: "Mr.Moderator", email: "mrmoderator@hotmail.com", password:"mod4life", moderator: true)
 # puts "generating 5 users"
@@ -23,11 +21,10 @@ User.create(first_name: "Mr", last_name: "Moderator", username: "Mr.Moderator", 
 
 # puts "generating 10 spots"
 
- Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Cascais", category: category.sample)
- Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Carcavelos", category: category.sample)
- Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Parede", category: category.sample)
- Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Oeiras", category: category.sample)
- Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Lisboa", category: category.sample)
+ #Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Carcavelos", category: category.sample)
+ #Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Parede", category: category.sample)
+ #Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Oeiras", category: category.sample)
+ #Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Lisboa", category: category.sample)
 #Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "barcelona", category: category.sample)
 # Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Madrid", category: category.sample)
 # Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Paris", category: category.sample)
@@ -36,10 +33,10 @@ User.create(first_name: "Mr", last_name: "Moderator", username: "Mr.Moderator", 
 # Spot.create(user: User.all.sample, name: Faker::Dessert.flavor, description: Faker::Quotes::Shakespeare.hamlet_quote, address: "Rua do Instituto Bacteriológico 8, Lisboa", category: category.sample)
 # puts "Success!"
 
-# puts "generating 30 reviews"
-# 30.times do
-#   Review.create(user: User.all.sample, spot: Spot.all.sample, description: Faker::Quotes::Shakespeare.hamlet_quote, title: Faker::Name.name, rating: rand(0..5))
-# end
+ #puts "generating 30 reviews"
+ #30.times do
+  # Review.create(user: User.all.sample, spot: Spot.all.sample, description: Faker::Quotes::Shakespeare.hamlet_quote, title: Faker::Name.name, rating: rand(0..5))
+#end 
 # puts "Success!"
 
 
@@ -47,4 +44,4 @@ User.create(first_name: "Mr", last_name: "Moderator", username: "Mr.Moderator", 
 # 100.times do
 #   Like.create(user: User.all.sample, review: Review.all.sample)
 # end
-puts "Success!"
+# puts "Success!"
