af63f0a1d99eadfd5a52a67f0f5bdd67cc758b7d
Fabio Freire <fabitwink@hotmail.com>
2021-03-10 10:53:51 +0000

    work in progress banning users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_100555) do
+ActiveRecord::Schema.define(version: 2021_03_10_095430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_100555) do
     t.string "last_name"
     t.string "username"
     t.boolean "moderator", default: false
+    t.boolean "banned", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
